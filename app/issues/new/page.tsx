d43c3c8880dab7e910c0436be3bde7675a2ea737
UbongJacob <ubongjacob13@gmail.com>
2023-10-26 22:15:49 +0100

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -25,6 +26,15 @@ const NewIssuesPage = () => {
   });
   const router = useRouter();
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    setSubmitting(true);
+    const result = await createIssue(data);
+    setSubmitting(false);
+    if (result.ok) router.replace("/issues");
+    else setError(result.problem);
+  });
 
   return (
     <div className="max-w-xl">
@@ -33,14 +43,7 @@ const NewIssuesPage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          const result = await createIssue(data);
-          if (result.ok) router.replace("/issues");
-          else setError(result.problem);
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
@@ -53,7 +56,9 @@ const NewIssuesPage = () => {
           )}
         ></Controller>
         <ErrorMessage> {errors?.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
