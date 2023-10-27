6261137091ed3f3f8953f5127a90022ca982059e
UbongJacob <ubongjacob13@gmail.com>
2023-10-27 12:27:33 +0100

    Disabled SSR for loading markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
-import "easymde/dist/easymde.min.css";
 import { createIssue } from "@/app/api/client/issues";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 export type IssueForm = z.infer<typeof createIssueSchema>;
 
-const NewIssuesPage = async () => {
+const NewIssuesPage = () => {
   const {
     register,
     control,
